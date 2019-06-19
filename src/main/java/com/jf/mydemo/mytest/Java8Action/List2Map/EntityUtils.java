3de097fc764ca0da73c77b432e2f4f1270fddedd
Monkey-D-Jie <liuyujunqi@163.com>
2019-06-19 09:54:03 +0800

    新增 实体和Map相互转换的工具类

---
+++ src/main/java/com/jf/mydemo/mytest/Java8Action/List2Map/EntityUtils.java
@@ -0,0 +1,73 @@
+package com.jf.mydemo.mytest.Java8Action.List2Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: Wangjie
+ * @Date: 2019-06-19 09:45
+ * @Description:
+ * 转载自：
+ * java 实体对象与Map之间的转换工具类 - JohnLiu的博客 - CSDN博客
+https://blog.csdn.net/m940034240/article/details/81118091
+
+ * To change this template use File | Settings | File and Templates.
+ */
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EntityUtils {
+    /**
+     * 实体类转Map
+     * @param object
+     * @return
+     */
+    public static Map<String, Object> entityToMap(Object object) {
+        Map<String, Object> map = new HashMap();
+        for (Field field : object.getClass().getDeclaredFields()){
+            try {
+                boolean flag = field.isAccessible();
+                field.setAccessible(true);
+                Object o = field.get(object);
+                map.put(field.getName(), o);
+                field.setAccessible(flag);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return map;
+    }
+
+    /**
+     * Map转实体类
+     * @param map 需要初始化的数据，key字段必须与实体类的成员名字一样，否则赋值为空
+     * @param entity  需要转化成的实体类
+     * @return
+     */
+    public static <T> T mapToEntity(Map<String, Object> map, Class<T> entity) {
+        T t = null;
+        try {
+            t = entity.newInstance();
+            for(Field field : entity.getDeclaredFields()) {
+                if (map.containsKey(field.getName())) {
+                    boolean flag = field.isAccessible();
+                    field.setAccessible(true);
+                    Object object = map.get(field.getName());
+                    if (object!= null && field.getType().isAssignableFrom(object.getClass())) {
+                        field.set(t, object);
+                    }
+                    field.setAccessible(flag);
+                }
+            }
+            return t;
+        } catch (InstantiationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return t;
+    }
+}
